0385e7a387265a872c3c483ceac9e82889d63cea
shreyanthatikonda <shreyanthatikonda3@gmail.com>
2025-09-03 11:31:23 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,14 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b = 15;
+		Byte bb=b;
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);
+	}	
+	
+	
+}	
